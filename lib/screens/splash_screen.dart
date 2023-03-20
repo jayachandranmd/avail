fc0f542f270bc81be28b6d32f279207ae58eeb8c
jayachandranmd <jayachandranmd.contact@gmail.com>
2023-03-20 11:59:48 +0530

    Splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'package:avail_itech_hackfest/screens/onboarding.dart';
+import 'package:avail_itech_hackfest/utils/colors.dart';
+import 'package:avail_itech_hackfest/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const OnboardingPage()),
+          (route) => false);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/logo/logo.png'),
+              sBoxH10,
+              Text(
+                'AVAIL',
+                style: TextStyle(
+                  color: black,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
